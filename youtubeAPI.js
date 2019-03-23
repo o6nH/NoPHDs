830287e8ec56881041ce5f02a93b850c3ffd6fe2
paulinat11 <paulinatrujillo11@gmail.com>
2019-03-23 14:09:38 -0700

    Two files

--- youtubeAPI.js
+++ youtubeAPI.js
@@ -394,6 +394,42 @@
 //   ]
 // }
 
+let num1 = Math.floor(Math.random() * 10);
+let num2 = Math.floor(Math.random() * 10);
+let operator = ["+", "-", "*",];
+let randOp = Math.floor(Math.random() * 3);
+let opPicked = operator[randOp];
+let n1 = document.getElementById("n1");
+let formAnswer = document.getElementById("formAnswer");
+
+function changeOp () {
+    n1.innerHTML = `${num1} ${opPicked} ${num2}`;
+}
+
+changeOp ();
+formButton.addEventListener('click', answer);
+
+
+function answer () {
+    let x = formAnswer.value;
+    if (opPicked === '+' && num1 + num2 == x) {
+        alert ('You got it correct!');
+    }
+    else if (opPicked === '-' && num1 - num2 == x) {
+        alert ('You got it correct!');
+    }
+    else if (opPicked === '*' && num1 * num2 == x) {
+        alert ('You got it correct!');
+    }
+    else if (opPicked === '/' && num1 / num2 == x) {
+        alert ('You got it correct!');
+    }
+    else {
+        alert ('Sorry, try again');
+    }
+}
+
+
 // Using Fecth
 const entertainURL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PL0FeOu0VtegcbplQ3bZ2SgjKVwsb-GxpJ&key=AIzaSyBWl800UrBjg3uVTtb9qPHTVuJywVDeaj0'
 const eduURL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PL0FeOu0VtegcevmxMM6hnmmlTFycHZ3Zb&key=AIzaSyBWl800UrBjg3uVTtb9qPHTVuJywVDeaj0'
