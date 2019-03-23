d7f4271b2bb09d07563d8f01b333db73c31c1b73
paulinat11 <paulinatrujillo11@gmail.com>
2019-03-23 12:10:12 -0700

    Changed ParamaterNames

--- youtubeAPI.js
+++ youtubeAPI.js
@@ -2,9 +2,16 @@
 var entertainURL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PL0FeOu0VtegcevmxMM6hnmmlTFycHZ3Zb&key=AIzaSyBWl800UrBjg3uVTtb9qPHTVuJywVDeaj0'
 const eduURL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PL0FeOu0VtegcevmxMM6hnmmlTFycHZ3Zb&key=AIzaSyBWl800UrBjg3uVTtb9qPHTVuJywVDeaj0'
 
-fetch(oauth2URL,{method:"GET"}).then(res => res.json())
-.then(response => {
-    bearerToken = response['access_token']
-    console.log('Success:', JSON.stringify(response));
-})
-.catch(error => console.error('Error:', error));
+fetch(entertainURL, { method: "GET" }).then(res => res.json())
+    .then(response => {
+        bearerToken = response['access_token']
+        console.log('Success:', JSON.stringify(response));
+    })
+    .catch(error => console.error('Error:', error));
+
+fetch(eduURL, { method: "GET" }).then(res => res.json())
+    .then(response => {
+        bearerToken = response['access_token']
+        console.log('Success:', JSON.stringify(response));
+    })
+    .catch(error => console.error('Error:', error));
