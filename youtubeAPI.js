ed356c9184201c34f88c0ad533ade3db58bb68fb
paulinat11 <paulinatrujillo11@gmail.com>
2019-03-23 11:29:49 -0700

    Added Youtube Example, Need Client ID?

--- youtubeAPI.js
+++ youtubeAPI.js
@@ -5,211 +5,143 @@
 // https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PL0FeOu0VtegcbplQ3bZ2SgjKVwsb-GxpJ&key=AIzaSyBWl800UrBjg3uVTtb9qPHTVuJywVDeaj0
 
 
-  /***** START BOILERPLATE CODE: Load client library, authorize user. *****/
+<script>
 
-  // Global variables for GoogleAuth object, auth status.
-  var GoogleAuth;
+/***** START BOILERPLATE CODE: Load client library, authorize user. *****/
 
-  /**
-   * Load the API's client and auth2 modules.
-   * Call the initClient function after the modules load.
-   */
-  function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-  }
+// Global variables for GoogleAuth object, auth status.
+var GoogleAuth;
 
-  function initClient() {
-    // Initialize the gapi.client object, which app uses to make API requests.
-    // Get API key and client ID from API Console.
-    // 'scope' field specifies space-delimited list of access scopes
+/**
+ * Load the API's client and auth2 modules.
+ * Call the initClient function after the modules load.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
 
-    gapi.client.init({
-        'clientId': 'REPLACE_ME',
-        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-        'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
-    }).then(function () {
-      GoogleAuth = gapi.auth2.getAuthInstance();
+function initClient() {
+  // Initialize the gapi.client object, which app uses to make API requests.
+  // Get API key and client ID from API Console.
+  // 'scope' field specifies space-delimited list of access scopes
 
-      // Listen for sign-in state changes.
-      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+  gapi.client.init({
+      'clientId': '#####',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+      'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
+  }).then(function () {
+    GoogleAuth = gapi.auth2.getAuthInstance();
 
-      // Handle initial sign-in state. (Determine if user is already signed in.)
-      setSigninStatus();
+    // Listen for sign-in state changes.
+    GoogleAuth.isSignedIn.listen(updateSigninStatus);
 
-      // Call handleAuthClick function when user clicks on "Authorize" button.
-      $('#execute-request-button').click(function() {
-        handleAuthClick(event);
-      }); 
-    });
-  }
-
-  function handleAuthClick(event) {
-    // Sign user in after click on auth button.
-    GoogleAuth.signIn();
-  }
-
-  function setSigninStatus() {
-    var user = GoogleAuth.currentUser.get();
-    isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
-    // Toggle button text and displayed statement based on current auth status.
-    if (isAuthorized) {
-      defineRequest();
-    }
-  }
-
-  function updateSigninStatus(isSignedIn) {
+    // Handle initial sign-in state. (Determine if user is already signed in.)
     setSigninStatus();
-  }
 
-  function createResource(properties) {
-    var resource = {};
-    var normalizedProps = properties;
-    for (var p in properties) {
-      var value = properties[p];
-      if (p && p.substr(-2, 2) == '[]') {
-        var adjustedName = p.replace('[]', '');
-        if (value) {
-          normalizedProps[adjustedName] = value.split(',');
-        }
-        delete normalizedProps[p];
+    // Call handleAuthClick function when user clicks on "Authorize" button.
+    $('#execute-request-button').click(function() {
+      handleAuthClick(event);
+    }); 
+  });
+}
+
+function handleAuthClick(event) {
+  // Sign user in after click on auth button.
+  GoogleAuth.signIn();
+}
+
+function setSigninStatus() {
+  var user = GoogleAuth.currentUser.get();
+  isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
+  // Toggle button text and displayed statement based on current auth status.
+  if (isAuthorized) {
+    defineRequest();
+  }
+}
+
+function updateSigninStatus(isSignedIn) {
+  setSigninStatus();
+}
+
+function createResource(properties) {
+  var resource = {};
+  var normalizedProps = properties;
+  for (var p in properties) {
+    var value = properties[p];
+    if (p && p.substr(-2, 2) == '[]') {
+      var adjustedName = p.replace('[]', '');
+      if (value) {
+        normalizedProps[adjustedName] = value.split(',');
       }
+      delete normalizedProps[p];
     }
-    for (var p in normalizedProps) {
-      // Leave properties that don't have values out of inserted resource.
-      if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
-        var propArray = p.split('.');
-        var ref = resource;
-        for (var pa = 0; pa < propArray.length; pa++) {
-          var key = propArray[pa];
-          if (pa == propArray.length - 1) {
-            ref[key] = normalizedProps[p];
-          } else {
-            ref = ref[key] = ref[key] || {};
-          }
+  }
+  for (var p in normalizedProps) {
+    // Leave properties that don't have values out of inserted resource.
+    if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
+      var propArray = p.split('.');
+      var ref = resource;
+      for (var pa = 0; pa < propArray.length; pa++) {
+        var key = propArray[pa];
+        if (pa == propArray.length - 1) {
+          ref[key] = normalizedProps[p];
+        } else {
+          ref = ref[key] = ref[key] || {};
         }
-      };
-    }
-    return resource;
-  }
-
-  function removeEmptyParams(params) {
-    for (var p in params) {
-      if (!params[p] || params[p] == 'undefined') {
-        delete params[p];
       }
-    }
-    return params;
+    };
   }
+  return resource;
+}
 
-  function executeRequest(request) {
-    request.execute(function(response) {
-      console.log(response);
+function removeEmptyParams(params) {
+  for (var p in params) {
+    if (!params[p] || params[p] == 'undefined') {
+      delete params[p];
+    }
+  }
+  return params;
+}
+
+function executeRequest(request) {
+  request.execute(function(response) {
+    console.log(response);
+  });
+}
+
+function buildApiRequest(requestMethod, path, params, properties) {
+  params = removeEmptyParams(params);
+  var request;
+  if (properties) {
+    var resource = createResource(properties);
+    request = gapi.client.request({
+        'body': resource,
+        'method': requestMethod,
+        'path': path,
+        'params': params
+    });
+  } else {
+    request = gapi.client.request({
+        'method': requestMethod,
+        'path': path,
+        'params': params
     });
   }
-
-  function buildApiRequest(requestMethod, path, params, properties) {
-    params = removeEmptyParams(params);
-    var request;
-    if (properties) {
-      var resource = createResource(properties);
-      request = gapi.client.request({
-          'body': resource,
-          'method': requestMethod,
-          'path': path,
-          'params': params
-      });
-    } else {
-      request = gapi.client.request({
-          'method': requestMethod,
-          'path': path,
-          'params': params
-      });
-    }
-    executeRequest(request);
-  }
-
-  /***** END BOILERPLATE CODE *****/
-
-// Define some variables used to remember state.
-var playlistId, nextPageToken, prevPageToken;
-
-// After the API loads, call a function to get the uploads playlist ID.
-function handleAPILoaded() {
-  requestUserUploadsPlaylistId();
+  executeRequest(request);
 }
 
-// Call the Data API to retrieve the playlist ID that uniquely identifies the
-// list of videos uploaded to the currently authenticated user's channel.
-function requestUserUploadsPlaylistId() {
-  // See https://developers.google.com/youtube/v3/docs/channels/list
-  var request = gapi.client.youtube.channels.list({
-    mine: true,
-    part: 'contentDetails'
-  });
-  request.execute(function(response) {
-    playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
-    requestVideoPlaylist(playlistId);
-  });
+/***** END BOILERPLATE CODE *****/
+
+
+function defineRequest() {
+  // See full sample for buildApiRequest() code, which is not 
+// specific to a particular API or API method.
+
+buildApiRequest('GET',
+              '/youtube/v3/playlistItems',
+              {'maxResults': '25',
+               'part': 'snippet,contentDetails',
+               'playlistId': 'PL0FeOu0VtegcbplQ3bZ2SgjKVwsb-GxpJ'});
+
 }
-
-// Retrieve the list of videos in the specified playlist.
-function requestVideoPlaylist(playlistId, pageToken) {
-  $('#video-container').html('');
-  var requestOptions = {
-    playlistId: playlistId,
-    part: 'snippet',
-    maxResults: 10
-  };
-  if (pageToken) {
-    requestOptions.pageToken = pageToken;
-  }
-  var request = gapi.client.youtube.playlistItems.list(requestOptions);
-  request.execute(function(response) {
-    // Only show pagination buttons if there is a pagination token for the
-    // next or previous page of results.
-    nextPageToken = response.result.nextPageToken;
-    var nextVis = nextPageToken ? 'visible' : 'hidden';
-    $('#next-button').css('visibility', nextVis);
-    prevPageToken = response.result.prevPageToken
-    var prevVis = prevPageToken ? 'visible' : 'hidden';
-    $('#prev-button').css('visibility', prevVis);
-
-    var playlistItems = response.result.items;
-    if (playlistItems) {
-      $.each(playlistItems, function(index, item) {
-        displayResult(item.snippet);
-      });
-    } else {
-      $('#video-container').html('Sorry you have no uploaded videos');
-    }
-  });
-}
-
-// Create a listing for a video.
-function displayResult(videoSnippet) {
-  var title = videoSnippet.title;
-  var videoId = videoSnippet.resourceId.videoId;
-  $('#video-container').append('<p>' + title + ' - ' + videoId + '</p>');
-}
-
-// Retrieve the next page of videos in the playlist.
-function nextPage() {
-  requestVideoPlaylist(playlistId, nextPageToken);
-}
-
-// Retrieve the previous page of videos in the playlist.
-function previousPage() {
-  requestVideoPlaylist(playlistId, prevPageToken);
-}
-
-
-/* Main Function */
-// generate math question and display
-// if math question correct display playlistId()
-//// get playlistID for entertaining playlist
-//requestVideoPlaylist('PL0FeOu0VtegcbplQ3bZ2SgjKVwsb-GxpJ','');
-// else math question is wrong and need to re-generate and re-display function
-//// get playlistID for educational playlist
-//requestVideoPlaylist('PL0FeOu0VtegcevmxMM6hnmmlTFycHZ3Zb','');
-
-handleAPILoaded()
+</script>
